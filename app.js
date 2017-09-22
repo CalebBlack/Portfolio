da0b4c89e8dddbc85ddbd8589eb6961716940b27
CalebBlack <caleb@polished.tech>
2017-09-22 17:24:09 -0600

    Update app.js

--- app.js
+++ app.js
@@ -40,25 +40,51 @@ class Background extends React.Component {
     return (<div className='backgroundHolder'><img id='background' src={this.props.source || ''}/></div>)
   }
 }
-class Eye extends React.Component {
+
+class Eye extends Component {
   constructor(){
     super();
     this.props = {};
+    this.state = {hovered: false, blinking: false};
     this.makeMoving = this.makeMoving.bind(this);
+    this.makeBlinking = this.makeBlinking.bind(this);
+  }
+  componentDidMount(){
+    if (this.eye) {
+      this.makeMoving(this.eye);
+      this.makeBlinking(this.eye);
+    }
   }
   render(){
-    return (<div ref={(eye)=>{this.makeMoving(eye)}} className={this.props.side ? 'eye '+this.props.side.trim() : 'eye'}/>)
+    return (<div ref={(eye)=>{this.eye = eye}} style={{opacity:(this.state.hovered || this.state.blinking || false) ? 1 : 0}} className={this.props.side ? 'eye '+this.props.side.trim() : 'eye'}/>)
   }
-  makeMoving(eyeDiv,radius){
+  makeBlinking(eyeDiv){
+    const blink = ()=>{
+      this.setState(Object.assign({},this.state,{blinking:true}));
+      setTimeout(()=>{
+        this.setState(Object.assign({},this.state,{blinking:false}));
+      },2000);
+    };
+    blink();
+    setInterval(blink,10000);
+  }
+  makeMoving(eyeDiv){
     const computedStyle = window.getComputedStyle(eyeDiv,null);
     var original = window.getComputedStyle(eyeDiv,null).getPropertyValue(this.props.side);
     original = parseInt(original.substring(0,original.length-2));
-    console.log(this.props);
     const gitdisplay = document.getElementById('gitdisplay');
+    gitdisplay.addEventListener('mousemove',()=>{
+      if (this.state.hovered !== true) {
+        this.setState(Object.assign({},this.state,{hovered:true}));
+      }
+    });
+    gitdisplay.addEventListener('mouseout',()=> {
+      if (this.state.hovered !== false) {
+        this.setState(Object.assign({},this.state,{hovered:false}));
+      }
+    });
     if (gitdisplay) {
-      console.log('git',gitdisplay.offsetWidth, gitdisplay.offsetHeight);
-      gitdisplay.addEventListener('mousemove',(event)=>{
-        //console.log(event.clientX,event.clientY)
+      document.addEventListener('mousemove',(event)=>{
         const [x, y] = getPosition(eyeDiv);
         var mouseX = event.clientX - x;
         var mouseY = event.clientY - y;
@@ -70,6 +96,8 @@ class Eye extends React.Component {
     }
   }
 }
+
+
 function getPosition(element) {
     for (var x=0, y=0; element != null; x += element.offsetLeft, y += element.offsetTop, element = element.offsetParent);
     return [x,y];
